db929f94b6c928ab82966a395b05971ab04b6748
ilvolofancommunity <ilvolomusic425@gmail.com>
2026-07-22 10:01:35 +0100

    Improve live news data structure

--- scripts/update-news.py
+++ scripts/update-news.py
@@ -5,13 +5,14 @@ feed = feedparser.parse("https://news.google.com/rss/search?q=IL+VOLO")
 
 news = []
 
-for item in feed.entries[:5]:
+for item in feed.entries[:6]:
     news.append({
         "title": item.title,
         "date": item.published,
+        "summary": getattr(item, "summary", ""),
         "link": item.link,
         "image": ""
     })
 
-with open("news.json", "w") as f:
-    json.dump(news, f, indent=4)
+with open("news.json", "w", encoding="utf-8") as f:
+    json.dump(news, f, indent=4, ensure_ascii=False)
