17ad4790d2fb317e6677e93bc0c2a95a004326cb
ilvolofancommunity <ilvolomusic425@gmail.com>
2026-07-22 15:10:51 +0100

    Fix news feed processing

--- scripts/update-news.py
+++ scripts/update-news.py
@@ -4,6 +4,8 @@ import re
 from email.utils import parsedate_to_datetime
 
 feed = feedparser.parse(
+if not feed.entries:
+    raise Exception("No news entries found in RSS feed")
     "https://news.google.com/rss/search?q=IL+VOLO&hl=en-US&gl=US&ceid=US:en"
 )
 
@@ -19,7 +21,7 @@ from datetime import datetime
 
 today = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
 
-for item in feed.entries[:6]:
+for item in entries[:6]:
     news.append({
         "title": item.title,
         "date": parsedate_to_datetime(item.published).strftime("%d %B %Y"),
