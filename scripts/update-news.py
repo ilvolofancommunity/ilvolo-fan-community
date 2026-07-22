210f88c8988755ff80e86e43ac3d575c54800f10
ilvolofancommunity <ilvolomusic425@gmail.com>
2026-07-22 15:12:21 +0100

    Debug news feed

--- scripts/update-news.py
+++ scripts/update-news.py
@@ -4,10 +4,10 @@ import re
 from email.utils import parsedate_to_datetime
 
 feed = feedparser.parse(
-if not feed.entries:
-    raise Exception("No news entries found in RSS feed")
     "https://news.google.com/rss/search?q=IL+VOLO&hl=en-US&gl=US&ceid=US:en"
 )
+
+print("Number of entries:", len(feed.entries))
 
 news = []
 
@@ -17,17 +17,13 @@ entries = sorted(
     reverse=True
 )
 
-from datetime import datetime
-
-today = datetime.utcnow().strftime("%a, %d %b %Y %H:%M:%S GMT")
-
 for item in entries[:6]:
     news.append({
         "title": item.title,
         "date": parsedate_to_datetime(item.published).strftime("%d %B %Y"),
         "summary": re.sub("<.*?>", "", getattr(item, "summary", ""))[:180] + "...",
         "link": item.link,
-        "image": "https://upload.wikimedia.org/wikipedia/commons/3/3b/Il_Volo_2015.jpg"
+        "image": "https://upload.wikimedia.org/wikipedia/commons/6/6b/Music_Notes.svg"
     })
 
 with open("news.json", "w", encoding="utf-8") as f:
